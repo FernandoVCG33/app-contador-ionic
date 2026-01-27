063a8d1d6b1ec19d7f71abd8ea148be836fc1bf6
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2026-01-27 19:58:32 -0400

    eliminar IonToolbar del módulo de la página principal

--- counter-app/src/app/home/home.page.ts
+++ counter-app/src/app/home/home.page.ts
@@ -5,7 +5,7 @@ import {IonHeader, IonToolbar, IonTitle, IonContent, IonButton} from '@ionic/ang
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
+  imports: [IonHeader,IonContent, IonButton],
 })
 export class HomePage {
   num:number=0;
