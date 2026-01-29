352beeffc49feeb94c388a6edbfc29978d3fdc68
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2026-01-29 18:02:11 -0400

    agregado ícono al botón en el encabezado de la página principal

--- counter-app/src/app/home/home.page.ts
+++ counter-app/src/app/home/home.page.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import {IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonFooter} from '@ionic/angular/standalone';
+import {IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonFooter, IonIcon} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonContent, IonButton, IonFooter],
+  imports: [IonHeader, IonContent, IonButton, IonFooter, IonIcon],
 })
 export class HomePage {
   num:number=0;
